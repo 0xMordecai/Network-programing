e8b38721f5871f0381aa60c88452743c0cd1348d
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-08 22:16:05 +0100

    update

--- building-api-client/APIserver.go
+++ building-api-client/APIserver.go
@@ -1,6 +1,13 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"net/http"
+)
 
 var tocken string
 var randomGen *rand.Rand
+
+func random(w http.ResponseWriter, r *http.Request) {
+
+}
