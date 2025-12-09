7c8b2c1d137346aa28c983d24f25646c431698d3
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-09 21:33:15 +0100

    Update

---
+++ building-api-client/client/app/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/kings5layer/API-PROGRAMING/building-api-client/client"
+)
+
+func main() {
+	c := client.NewClient("http://localhost:8888")
+	if err := c.Login("user", "password"); err != nil {
+		log.Fatal(err)
+	}
+}
