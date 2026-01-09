2c55864f010324e86e1b9a3482d42aeb031992f6
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-01-09 19:21:20 +0100

    update handleLogin function

--- Designing-REST-API/main.go
+++ Designing-REST-API/main.go
@@ -78,7 +78,7 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"token": token})
 	}
-
+	w.WriteHeader(http.StatusUnauthorized)
 }
 
 // CRUD Handlers
