5aed697191b3a613e5d516e796f6a0865131cd13
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-02-01 00:30:53 +0100

    dB.go[update]

--- DB-Access/dB.go
+++ DB-Access/dB.go
@@ -1,7 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	_ "database/sql"
+	"fmt"
 	_ "fmt"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
